1fc0f24699041528cd170100e350e5696dcdd96f
EmmanuelAdams <oluwaseunadams17@gmail.com>
2023-04-28 20:47:53 +0100

    backend apiConfig

--- src/backendCalls.js
+++ src/backendCalls.js
@@ -6,13 +6,8 @@ export const loginCall = async (
 ) => {
   dispatch({ type: 'LOGIN_START' });
   try {
-<<<<<<< HEAD
-    const res = await axios.post(
-      'https://paychat-api.onrender.com/api/auth/login',
-=======
     const res = await api.post(
       '/auth/login',
->>>>>>> 22418d8 (backend apiConfig)
       userCredentials
     );
     dispatch({ type: 'LOGIN_SUCCESS', payload: res.data });
