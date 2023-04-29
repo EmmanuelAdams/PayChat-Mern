f58cedf8ff5efb2be2c486168f1e16affce1ef7b
EmmanuelAdams <oluwaseunadams17@gmail.com>
2023-04-29 19:47:49 +0100

    error boundary

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${
-    process.env.REACT_APP_API_URL
-  }?_=${new Date().getTime()}`,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
